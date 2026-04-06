bd3ad022787289ed44bd4497339d4550b1e1c91e
err <edison7500@gmail.com>
2026-04-06 21:30:46 +0800

    update

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ if __name__ == "__main__":
     index = st.Page("ui/indices.py", title="指数")
     etf = st.Page("ui/etf.py", title="ETF")
     cron = st.Page("tools/cron.py", title="自动化行情同步任务")
-    asset_manager = st.Page("tools/asset_manager.py", title="资产管理")
+    asset_manager = st.Page("ui/admin/asset_manager.py", title="资产管理")
 
     pg = st.navigation(
         {
             "📈 量化投研 Dashboard": [equity, index, etf],
-            "Tools": [cron, asset_manager],
+            "Tools": [cron],
+            "Admin": [asset_manager],
         }
     )
     pg.run()
